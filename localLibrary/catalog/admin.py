7f09984d606a1d85c03f2c740e825d44f390c1db
lisantov <lisantov@yandex.ru>
2025-11-24 11:49:06 +0700

    Add: Admin view layout for models

--- localLibrary/catalog/admin.py
+++ localLibrary/catalog/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Genre, Language, Book, BookInstance, Author
 
-# Register your models here.
+admin.site.register(Genre)
+admin.site.register(Language)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    list_display = ('id', 'due_back', 'status')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Доступность', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
