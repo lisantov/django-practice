a8397af66ab023c5ba10b1c5a0b518335ee2eb63
lisantov <lisantov@yandex.ru>
2025-11-24 11:04:24 +0700

    Add: Basic models

--- localLibrary/catalog/models.py
+++ localLibrary/catalog/models.py
@@ -1,3 +1,116 @@
 from django.db import models
+from django.urls import reverse
+from django.db.models import UniqueConstraint
+from django.db.models.functions import Lower
+import uuid
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        help_text="Введите жанр книги (например: Научная фантастика)",
+    )
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("genre-detail", args=[str(self.id)])
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                Lower("name"),
+                name="genre_name_case_insensitive_unique",
+                violation_error_message="Жанр уже существует",
+            )
+        ]
+
+class Language(models.Model):
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        help_text="Введите язык книги (например: Русский)",
+    )
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("language-detail", args=[str(self.id)])
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                Lower("name"),
+                name="language_name_case_insensitive_unique",
+                violation_error_message="Язык уже существует",
+            )
+        ]
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey("Author", on_delete=models.RESTRICT, null=True)
+    summary = models.TextField(
+        max_length=1000,
+        help_text="Введите описание книги"
+    )
+    isbn = models.CharField(
+        'ISBN',
+        max_length=13,
+        help_text="13 символьный номер <a href='https://www.isbn-international.org/content/what-isbn'>ISBN</a>"
+    )
+    genre = models.ManyToManyField(Genre, help_text="Выберите жанр книги")
+    language = models.ForeignKey(Language, help_text="Выберите язык книги", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[str(self.id)])
+
+class BookInstance(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        help_text="Уникальный ключ для этой книги"
+    )
+    book = models.ForeignKey("Book", on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text="Статус доступности книги"
+    )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        return f'{self.id} ({self.book.title})'
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        return reverse("author-detail", args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
