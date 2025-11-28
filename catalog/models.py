f671884db23ba012b78ceab27ce08db9de9f6bcd
lisantov <lisantov@yandex.ru>
2025-11-28 14:03:41 +0700

    Add: Basic models. Template for html

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100,
+        help_text="Имя категории",
+        verbose_name="Имя категории"
+    )
+
+class Request(models.Model):
+    title = models.CharField(
+        max_length=200,
+        help_text="Имя заявки",
+        verbose_name="Имя заявки"
+    )
+    description = models.TextField(
+        max_length=1000,
+        help_text="Описание заявки",
+        verbose_name="Описание заявки"
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        help_text="Категория заявки",
+        verbose_name="Категория заявки"
+    )
+    photo = models.ImageField(
+        upload_to="images/",
+        help_text="Фотография заявки",
+        verbose_name="Фотография заявки"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания заявки",
+        help_text="Дата создания заявки"
+    )
+
+    CATEGORY_CHOICES = (
+        ('n', 'Новая'),
+        ('o', 'Принято в работу'),
+        ('d', 'Выполнено')
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=CATEGORY_CHOICES,
+        default='n',
+        verbose_name="Статус заявки",
+        help_text="Статус заявки"
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']
+
+class AdvancedUser(AbstractUser):
+    fio = models.CharField(
+        max_length=300,
+        verbose_name="ФИО пользователя",
+        help_text="ФИО"
+    )
+    email = models.EmailField(
+        max_length=100,
+        verbose_name="Электронная почта пользователя",
+        help_text="Электронная почта"
+    )
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Request
 
-# Create your views here.
+def index(request):
+    num_ongoing = Request.objects.filter(status__exact='o').count()
+    last_done = Request.objects.filter(status__exact='d')[:4]
+
+    context = {
+        'num_ongoing': num_ongoing,
+        'last_done': last_done,
+    }
+
+    return render(request, 'catalog/index.html', context=context)
