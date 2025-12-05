2d540b32bf1f41a73e5ccddbf6bb99465c50e8dd
lisantov <lisantov@yandex.ru>
2025-12-05 09:42:50 +0700

    Add: Viewsets instead views. Created router

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,39 @@
 from django.urls.conf import path, include
 from . import views
+from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
 
-urlpatterns = [
-    path('snippets/', views.SnippetList.as_view(), name='snippet_list'),
-    path('snippets/<int:pk>', views.SnippetDetail.as_view(), name='snippet_detail'),
-    path('users/', views.UserList.as_view(), name='snippet_list'),
-    path('users/<int:pk>', views.UserDetail.as_view(), name='snippet_detail'),
-    path('auth/', include('rest_framework.urls'), name='auth'),
-]
+from .views import SnippetViewSet, UserViewSet
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+snippet_list = SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+snippet_detail = SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+snippet_highlight = SnippetViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
+
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
